3585dbfdee0a5ae950970472de61c81a4f237bf0
Burak Yılmaz <burakk.ylmaz@outlook.com>
2019-08-03 12:13:16 +0300

    what is the variable, assinging value to variables, standart data types

--- PythonApplication.Intro/PythonApplication.Intro.py
+++ PythonApplication.Intro/PythonApplication.Intro.py
@@ -1,2 +1,97 @@
 
+"""
 
+DATA TYPES
+
+WHAT IS THE VARIABLE?
+
+Variables are nothing but reserved memory locations to store values. This means that when you create a variable you reserve some space in memory. Based on the data type of a variable, the interpreter allocates memory and decides what can be stored in the reserved memory. Therefore, by assigning different data types to variables, you can store integers, decimals or characters in these variables.
+
+ASSINGING VALUES TO VARIABLES
+
+Python variables do not need explicit declaration to reserve memory space. The declaration happens automatically when you assign a value to a variable. The equal sign (=) is used to assign values to variables.
+
+The operand to the left of the = operator is the name of the variable and the operand to the right of the = operator is the value stored in the variable. 
+
+For Example
+counter = 100         # An integer assignment
+miles   = 1000.0      # A floating point
+name    = "John"      # A string
+
+print counter  # print is output code
+print miles
+print name
+
+STANDART DATA TYPES
+
+Numbers, String, List, Tuple, Dictionary
+
+Numbers: Number data types store numeric values. Number objects are created when you assign a value to them. For example: int, long, float
+
+String: Strings in Python are identified as a contiguous set of characters represented in the quotation marks. Python allows for either pairs of single or double quotes. Subsets of strings can be taken using the slice operator ([ ] and [:] ) with indexes starting at 0 in the beginning of the string and working their way from -1 at the end.
+
+str = 'Hello World!'
+
+print str           # Prints complete string
+print str[0]        # Prints first character of the string
+print str[2:5]      # Prints characters starting from 3rd to 5th
+print str[2:]       # Prints string starting from 3rd character
+print str * 2       # Prints string two times
+print str + "TEST"  # Prints concatenated string
+
+Lists: Lists are the most versatile of Pythons compound data types. A list contains items separated by commas and enclosed within square brackets ([]). To some extent, lists are similar to arrays in C. One difference between them is that all the items belonging to a list can be of different data type.
+
+For Example
+
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+tinylist = [123, 'john']
+
+print list          # Prints complete list
+print list[0]       # Prints first element of the list
+print list[1:3]     # Prints elements starting from 2nd till 3rd 
+print list[2:]      # Prints elements starting from 3rd element
+print tinylist * 2  # Prints list two times
+print list + tinylist # Prints concatenated lists
+
+Tuples: A tuple is another sequence data type that is similar to the list. A tuple consists of a number of values separated by commas. Unlike lists, however, tuples are enclosed within parentheses.
+
+The main differences between lists and tuples are: Lists are enclosed in brackets ( [ ] ) and their elements and size can be changed, while tuples are enclosed in parentheses ( ( ) ) and cannot be updated. Tuples can be thought of as read-only lists. 
+
+For example 
+print tuple             # Prints complete list
+print tuple[0]          # Prints first element of the list
+print tuple[1:3]        # Prints elements starting from 2nd till 3rd 
+print tuple[2:]         # Prints elements starting from 3rd element
+print tinytuple * 2     # Prints list two times
+print tuple + tinytuple # Prints concatenated lists
+
+Dictionary: Python's dictionaries are kind of hash table type. They work like associative arrays or hashes found in Perl and consist of key-value pairs. A dictionary key can be almost any Python type, but are usually numbers or strings. Values, on the other hand, can be any arbitrary Python object.
+
+Dictionaries are enclosed by curly braces ({ }) and values can be assigned and accessed using square braces ([]). 
+
+For example
+dict = {}
+dict['one'] = "This is one"
+dict[2]     = "This is two"
+
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+
+print dict['one']       # Prints value for 'one' key
+print dict[2]           # Prints value for 2 key
+print tinydict          # Prints complete dictionary
+print tinydict.keys()   # Prints all the keys
+print tinydict.values() # Prints all the values
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+ 
